56bad75fba49eacbca1a62a94842675ca82463ac
Bhavya Khandelwal <bhavyakhandelwal210@gmail.com>
2026-02-16 20:54:30 +0530

    Second Commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.use((req,res)=>{
     res.end("Hello is my name");
 })
+app.use("/welcome",(req,res)=>{
+    res.end("Welcome My Friend")
+})
 app.listen(65400,()=>{
    console.log("Serve is Running ");
    
